5d236b5cbed1bf27f0c3b22fab036be1e624e758
Drizzle <i@drizzle.vip>
2021-03-24 12:45:21 +0800

    优化手机端界面显示

--- model/api.php
+++ model/api.php
@@ -1,5 +1,11 @@
 <?php
 require_once 'mysql.php';
-$db=new Mysql();
-$data=$db->table('data')->field('*')->where('Id>-1')->item();
-echo(json_encode($data));
+$type = null;
+if (isset($_GET['type']))
+    $type = $_GET['type'];
+$db = new Mysql();
+$data = $db->table('data')->field('*')->where('Id>-1')->item();
+if ($type == 'text')
+    exit($data['content']);
+else
+    echo(json_encode($data));
