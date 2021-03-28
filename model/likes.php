9d1876444224d55d85afc997561058353e4aa65b
Drizzle <i@drizzle.vip>
2021-03-28 15:48:20 +0800

    完善句子喜爱功能

--- model/likes.php
+++ model/likes.php
@@ -3,6 +3,6 @@ require_once 'mysql.php';
 $db = new Mysql();
 $id = $_GET['id'];
 $r = $db->table('data')->field('likes')->where("id=$id")->item();
-$r = $r['likes'];
-$db->table('data')->where("id=$id")->update(array('likes', $r + 1));
+$r['likes'] = $r['likes'] + 1;
+$db->table('data')->where("id=$id")->update($r);
 exit('谢谢哦');
